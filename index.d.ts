aa13126ab28e7b882f22aa7bc8887726f6998321
wchang28 <wchang28@hotmail.com>
2016-06-28 14:48:30 -0400

    added typescript support

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,5 @@
 export declare class SimpleMSSQL {
+	constructor(sqlConfig:any);
 	query(sqlString: string, params: any, done:(err: any, recordsets: any) => void) : void;
 	execute(storedProc: string, params: any, done:(err: any, recordsets: any) => void) : void;
 }
